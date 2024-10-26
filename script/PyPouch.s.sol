dd705cc3458d3447be5a57a0518fd25e612f933b
Michael Ghen <mike@mikeghen.com>
2024-10-26 19:43:58 -0400

    deploy script

--- script/PyPouch.s.sol
+++ script/PyPouch.s.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {PyPouch} from "../src/PyPouch.sol";
+import {PyPouch} from "src/PyPouch.sol";
 
+// PyPouch deployment script
+// 
+// forge script script/PyPouch.s.sol:PyPouchScript --rpc-url $RPC_URL --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY
+//
 contract PyPouchScript is Script {
     PyPouch public pyPouch;
 
@@ -13,11 +17,11 @@ contract PyPouchScript is Script {
         vm.startBroadcast();
 
         // Replace these addresses with the actual addresses for your deployment
-        address pyusdToken = address(0x466a756E9A7401B5e2444a3fCB3c2C12FBEa0a54);  // PYUSD token address
-        address aPYUSD = address(0x9daF8c91AEFAE50b9c0E69629D3F6Ca40cA3B3FE);  // aPYUSD token address
-        address poolAddressesProvider = address(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);  // Aave Pool Addresses Provider address
+        address pyusdToken = address(0x6c3ea9036406852006290770BEdFcAbA0e23A0e8);  // PYUSD token address
+        address aPYUSD = address(0x0C0d01AbF3e6aDfcA0989eBbA9d6e85dD58EaB1E);  // aPYUSD token address
+        address poolAddress = address(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);  // Aave Pool Addresses Provider address
 
-        pyPouch = new PyPouch(pyusdToken, aPYUSD, poolAddressesProvider);
+        pyPouch = new PyPouch(pyusdToken, aPYUSD, poolAddress);
 
         console.log("PyPouch deployed at:", address(pyPouch));
 
